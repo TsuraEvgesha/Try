8f1e8ad5887049a66acb9881ed9b3bed26293f84
Tsura Evgesha <primak.evgesha@mail.ru>
2022-12-05 19:25:22 +0700

    1

--- app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
+++ app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
@@ -18,19 +18,13 @@ interface PostDao {
     @Query(
         """
            UPDATE PostEntity SET
-                likes = likes + CASE WHEN liked THEN -1 ELSE 1 END,
-                liked = CASE WHEN liked THEN 0 ELSE 1 END
+                likes = likes + CASE WHEN likedByMe THEN -1 ELSE 1 END,
+                likedByMe = CASE WHEN likedByMe THEN 0 ELSE 1 END
            WHERE id = :id
         """)
     fun likeById(id: Long)
-    @Query(
-        """
-            UPDATE PostEntity SET
-            share = share+1
-            WHERE id= :id
-        """
-    )
-    fun shareById(id: Long)
+
+
     @Query("DELETE FROM PostEntity WHERE id = :id")
     fun removeById(id: Long)
 }
